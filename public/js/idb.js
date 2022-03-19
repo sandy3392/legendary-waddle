88cf432f4957fe3ae4d1f1ba7e42130b1232df58
Sandeep Vittala <applesandy33@gmail.com>
2022-03-19 18:37:24 -0400

    adding manifest.json

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,7 +8,7 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, 
+    // create an object store (table) called `new_dbtransaction`, 
     //set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_bdtransaction', { autoIncrement: true });
   };
@@ -21,7 +21,7 @@ request.onsuccess = function(event) {
     db = event.target.result;
   
     // check if app is online, if yes 
-    // run uploadPizza() function to send all local db data to api
+    // run uploaddbtransaction() function to send all local db data to api
     
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, 
@@ -35,12 +35,12 @@ request.onsuccess = function(event) {
     console.log(event.target.errorCode);
   };
 
-  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
+  // This function will be executed if we attempt to submit a new dbtransaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_bdtransaction'], 'readwrite');
   
-    // access the object store for `new_pizza`
+    // access the object store for `new_dbtransaction`
     const bdObjectStore = transaction.objectStore('new_bdtransaction');
   
     // add record to your store with add method
@@ -75,7 +75,7 @@ function uploadBdtransaction() {
             }
             // open one more transaction
             const transaction = db.transaction(['new_bdtransaction'], 'readwrite');
-            // access the new_pizza object store
+            // access the new_dbtransaction object store
             const bdObjectStore = transaction.objectStore('new_bdtransaction');
             // clear all items in your store
             bdObjectStore.clear();
